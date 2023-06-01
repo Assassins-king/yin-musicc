4445875a84e9d4f536f7a07fa03171ee133686d9
Assassins-king <1031563986@qq.com>
2023-06-01 14:44:56 +0800

    init

---
+++ music-server/src/main/java/com/example/yin/mapper/UserSupportMapper.java
@@ -0,0 +1,8 @@
+package com.example.yin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.yin.model.domain.UserSupport;
+
+public interface UserSupportMapper extends BaseMapper<UserSupport> {
+
+}
